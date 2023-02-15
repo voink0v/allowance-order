222967617c2acd7604d9987e318fe386ea72f9f2
Ilya Voinkov <voinkov.ilya@yandex.ru>
2023-02-15 22:48:13 +0300

    work day 1

--- src/main/java/StudentOrderValidator.java
+++ src/main/java/StudentOrderValidator.java
@@ -3,26 +3,40 @@ public class StudentOrderValidator {
         checkAll();
     }
 
-    public static void checkAll(){
-        checkCitiRegister();
-        checkWedding();
-        checkChildren();
-        checkStudent();
+    public static void checkAll() {
+        StudentOrder so = readStudentOrder();
+
+        checkCitiRegister(so);
+        checkWedding(so);
+        checkChildren(so);
+        checkStudent(so);
+
+        sendMail(so);
+
     }
 
-    public static void checkCitiRegister(){
+    public static StudentOrder readStudentOrder() {
+        StudentOrder so = new StudentOrder();
+        return so;
+    }
+
+    public static void checkCitiRegister(StudentOrder so) {
         System.out.println("CityRegister is running");
     }
 
-    public static void checkWedding(){
+    public static void checkWedding(StudentOrder so) {
         System.out.println("Wedding is running");
     }
 
-    public static void checkChildren(){
+    public static void checkChildren(StudentOrder so) {
         System.out.println("Children Check is running");
     }
 
-    public static void checkStudent(){
+    public static void checkStudent(StudentOrder so) {
         System.out.println("Студенты проверяются ");
+    }
+
+    public static void sendMail(StudentOrder so){
+
     }
 }
